c0f4ef8fb762522fb7bbd6c04b129cd1ca8b8d5b
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-28 18:33:42 -0300

    feat: :sparkles: Added image to the table

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -1,8 +1,11 @@
 import { MoreVertical, Star } from "lucide-react";
+import Image from "next/image";
+
+import cooper from "@/app/assets/mine-cooper.jpeg";
 
 export function Table() {
   return (
-    <table className="min-w-full divide-y divide-gray-200 shadow-lg border">
+    <table className="min-w-full divide-y divide-gray-200 shadow-lg border ">
       <thead className="bg-gray-50">
         <tr>
           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -25,10 +28,238 @@ export function Table() {
 
       <tbody className="bg-white divide-y divide-gray-200">
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">Mini Cooper 2020</td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
           <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
           <td className="px-6 py-4 whitespace-nowrap ">
-            <span className="bg-green-200 p-2 text-green-900 font-semibold  rounded-xl">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
+              {" "}
+              Available
+            </span>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex ">
+              {" "}
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <MoreVertical className="ml-4" />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
+          <td className="px-6 py-4 whitespace-nowrap ">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
+              {" "}
+              Available
+            </span>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex ">
+              {" "}
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <MoreVertical className="ml-4" />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
+          <td className="px-6 py-4 whitespace-nowrap ">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
+              {" "}
+              Available
+            </span>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex ">
+              {" "}
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <MoreVertical className="ml-4" />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
+          <td className="px-6 py-4 whitespace-nowrap ">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
+              {" "}
+              Available
+            </span>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex ">
+              {" "}
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <MoreVertical className="ml-4" />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
+          <td className="px-6 py-4 whitespace-nowrap ">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
+              {" "}
+              Available
+            </span>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex ">
+              {" "}
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <MoreVertical className="ml-4" />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
+          <td className="px-6 py-4 whitespace-nowrap ">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
+              {" "}
+              Available
+            </span>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex ">
+              {" "}
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+              <Star fill="" />
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <MoreVertical className="ml-4" />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            <div className=" flex items-center">
+              {" "}
+              <Image
+                className="rounded-sm me-2"
+                src={cooper}
+                alt="Mini Cooper"
+                width={81}
+                height={93}
+              />{" "}
+              Mini Cooper 2020
+            </div>
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">Jul 6 - Jul 12</td>
+          <td className="px-6 py-4 whitespace-nowrap ">
+            <span className="bg-emerald-100 p-2 text-emerald-900 font-semibold  rounded-xl">
               {" "}
               Available
             </span>
