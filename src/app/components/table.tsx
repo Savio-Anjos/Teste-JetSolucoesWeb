d1964b7b1e3f2cec8f84ce579dcc6ebdba843f3b
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-29 14:36:16 -0300

    fix: :bug: Fixed table rendering

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -7,7 +7,7 @@ import cooper from "@/app/assets/mine-cooper.jpeg";
 import { useEffect, useState } from "react";
 
 export function Table() {
-  const [isDesktop, setIsDesktop] = useState<boolean>(true);
+  const [isDesktop, setIsDesktop] = useState<boolean | null>(null);
 
   useEffect(() => {
     const handleResize = () => {
@@ -36,12 +36,9 @@ export function Table() {
             />
 
             <div className="absolute top-2 right-17 left-1 transform ">
-              <div
-              
-                className="flex rounded-full bg-green-800 items-center justify-center text-center p-1"
-              >
+              <div className="flex rounded-full bg-green-800 items-center justify-center text-center p-1">
                 <Check color="#FFF" size={12} />
-                </div>
+              </div>
             </div>
           </div>
           <div className="flex w-11/12 justify-between">
@@ -49,7 +46,7 @@ export function Table() {
               <h3 className="font-semibold mb-2 text-md">Mini Cooper 2020</h3>
               <span className="mb-3 font-light text-sm">
                 Next Reservation: 11/07
-              </span>   
+              </span>
               <div className="flex">
                 <Star size={18} fill="" />
                 <Star size={18} fill="" />
@@ -73,58 +70,17 @@ export function Table() {
             />
 
             <div className="absolute top-2 right-17 left-1 transform ">
-              <div
-              
-                className="flex rounded-full bg-green-800 items-center justify-center text-center p-1"
-              >
+              <div className="flex rounded-full bg-green-800 items-center justify-center text-center p-1">
                 <Check color="#FFF" size={12} />
-                </div>
-            </div>
-          </div>
-          <div className="flex w-11/12 justify-between">
-            <div className="flex flex-col">
-              <h3 className="font-semibold mb-2 text-md">Mini Cooper 2020</h3>
-              <span className="mb-3 font-light text-sm">
-                Next Reservation: 11/07
-              </span>   
-              <div className="flex">
-                <Star size={18} fill="" />
-                <Star size={18} fill="" />
-                <Star size={18} fill="" />
-                <Star size={18} fill="" />
-                <Star size={18} fill="" />
               </div>
             </div>
-            <div>
-              <MoreVertical />
-            </div>
-          </div>
-        </section>
-
-        <section className="flex justify-between p-3 flex-1 rounded-sm shadow-lg relative">
-          <div className="flex items-center me-4 relative">
-            <Image
-              className="rounded-sm flex"
-              src={cooper}
-              alt="Mini Cooper"
-              height={100}
-            />
-
-            <div className="absolute top-2 right-17 left-1 transform ">
-              <div
-              
-                className="flex rounded-full bg-green-800 items-center justify-center text-center p-1"
-              >
-                <Check color="#FFF" size={12} />
-                </div>
-            </div>
           </div>
           <div className="flex w-11/12 justify-between">
             <div className="flex flex-col">
               <h3 className="font-semibold mb-2 text-md">Mini Cooper 2020</h3>
               <span className="mb-3 font-light text-sm">
                 Next Reservation: 11/07
-              </span>   
+              </span>
               <div className="flex">
                 <Star size={18} fill="" />
                 <Star size={18} fill="" />
@@ -149,12 +105,9 @@ export function Table() {
             />
 
             <div className="absolute top-2 right-17 left-1 transform ">
-              <div
-              
-                className="flex rounded-full bg-green-800 items-center justify-center text-center p-1"
-              >
+              <div className="flex rounded-full bg-green-800 items-center justify-center text-center p-1">
                 <Check color="#FFF" size={12} />
-                </div>
+              </div>
             </div>
           </div>
           <div className="flex w-11/12 justify-between">
@@ -162,7 +115,7 @@ export function Table() {
               <h3 className="font-semibold mb-2 text-md">Mini Cooper 2020</h3>
               <span className="mb-3 font-light text-sm">
                 Next Reservation: 11/07
-              </span>   
+              </span>
               <div className="flex">
                 <Star size={18} fill="" />
                 <Star size={18} fill="" />
@@ -187,12 +140,9 @@ export function Table() {
             />
 
             <div className="absolute top-2 right-17 left-1 transform ">
-              <div
-              
-                className="flex rounded-full bg-green-800 items-center justify-center text-center p-1"
-              >
+              <div className="flex rounded-full bg-green-800 items-center justify-center text-center p-1">
                 <Check color="#FFF" size={12} />
-                </div>
+              </div>
             </div>
           </div>
           <div className="flex w-11/12 justify-between">
@@ -200,7 +150,42 @@ export function Table() {
               <h3 className="font-semibold mb-2 text-md">Mini Cooper 2020</h3>
               <span className="mb-3 font-light text-sm">
                 Next Reservation: 11/07
-              </span>   
+              </span>
+              <div className="flex">
+                <Star size={18} fill="" />
+                <Star size={18} fill="" />
+                <Star size={18} fill="" />
+                <Star size={18} fill="" />
+                <Star size={18} fill="" />
+              </div>
+            </div>
+            <div>
+              <MoreVertical />
+            </div>
+          </div>
+        </section>
+
+        <section className="flex justify-between p-3 flex-1 rounded-sm shadow-lg relative">
+          <div className="flex items-center me-4 relative">
+            <Image
+              className="rounded-sm flex"
+              src={cooper}
+              alt="Mini Cooper"
+              height={100}
+            />
+
+            <div className="absolute top-2 right-17 left-1 transform ">
+              <div className="flex rounded-full bg-green-800 items-center justify-center text-center p-1">
+                <Check color="#FFF" size={12} />
+              </div>
+            </div>
+          </div>
+          <div className="flex w-11/12 justify-between">
+            <div className="flex flex-col">
+              <h3 className="font-semibold mb-2 text-md">Mini Cooper 2020</h3>
+              <span className="mb-3 font-light text-sm">
+                Next Reservation: 11/07
+              </span>
               <div className="flex">
                 <Star size={18} fill="" />
                 <Star size={18} fill="" />
